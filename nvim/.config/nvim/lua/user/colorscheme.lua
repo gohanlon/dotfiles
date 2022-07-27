b50066768280b18ed4752c99917d495d43ef9a73
Galen O’Hanlon <gohanlon@icloud.com>
2022-07-27 11:31:49 -0700

    nvim: Add example for setting a non-configurable colorscheme

--- nvim/.config/nvim/lua/user/colorscheme.lua
+++ nvim/.config/nvim/lua/user/colorscheme.lua
@@ -23,3 +23,10 @@ gruvbox.setup({
   },
 })
 vim.cmd("colorscheme gruvbox")
+
+-- local colorscheme = "onedarker"
+-- local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+-- if not status_ok then
+--   vim.notify("colorscheme " .. colorscheme .. " not found!")
+--   return
+-- end
