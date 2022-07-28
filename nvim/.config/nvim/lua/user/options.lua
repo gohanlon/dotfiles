19e00fa0fef54b783206e2490449081abc61e0b6
Galen O’Hanlon <gohanlon@icloud.com>
2022-07-27 21:24:22 -0700

    nvim: Don't sync the unnamed register to the system clipboard

--- nvim/.config/nvim/lua/user/options.lua
+++ nvim/.config/nvim/lua/user/options.lua
@@ -1,7 +1,7 @@
 -- :help options
 local options = {
   backup = false,                          -- creates a backup file
-  clipboard = "unnamedplus",               -- allows neovim to access the system clipboard
+  clipboard = "",                          -- don't sync the unnamed register to the system clipboard
   cmdheight = 1,                           -- more space in the neovim command line for displaying messages
   completeopt = { "menuone", "noselect" }, -- mostly just for cmp
   conceallevel = 0,                        -- so that `` is visible in markdown files
